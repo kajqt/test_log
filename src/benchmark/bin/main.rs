efee6174ab65076f5b0b06e5675737fa68f87adb
root <xuhelios95@gmail.com>
2024-07-16 13:51:02 +0200

    ben

--- src/benchmark/bin/main.rs
+++ src/benchmark/bin/main.rs
@@ -10,6 +10,7 @@ use webpki_roots;
 use rustls::RootCertStore;
 use rustls::{ClientConfig};
 use std::path::Path;
+use std::time::Instant;
 // use rustls::HandshakeType::Certificate;
 // use rustls::Certificate;
 use rustls::pki_types::{CertificateDer, PrivateKeyDer, ServerName};
@@ -25,21 +26,22 @@ fn load_certs(filename: &str) -> Vec<CertificateDer<'static>> {
 }
 
 fn main() {
-    let mut socket = std::net::TcpStream::connect("www.google.com:443").unwrap();
+    
     let mut config = rustls::ClientConfig::builder();
     
     let mut root_store = RootCertStore::empty();
     root_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
-    println!("Number of items in root_store: {}", root_store.len());
+
+    //Loading Additional Certificates
     let file = File::open("/home/ubuntu/aprototype/certs/newcombine.crt").unwrap();
     let mut pem = std::io::BufReader::new(file);
     // let certs = rustls_pemfile::certs(&mut pem);
 
     // let mut pem = BufReader::new(File::open(cafile)?);
-        for cert in rustls_pemfile::certs(&mut pem) {
-            root_store.add(cert.unwrap()).unwrap();
-        }
-        println!("Number of items in root_store: {}", root_store.len());
+    for cert in rustls_pemfile::certs(&mut pem) {
+        root_store.add(cert.unwrap()).unwrap();
+    }
+        // println!("Number of items in root_store: {}", root_store.len());
     // for r in certs.into_iter(){
     //     match r {
     //         Ok(cert) => {
@@ -64,8 +66,11 @@ fn main() {
 
     // let mut client = rustls::ClientSession::new(&arc, dns_name);
     let mut tls = rustls::Stream::new(&mut conn, &mut sock);
+    let  num_requests = 0;
+    let start = Instant::now();
+    //Sending HTTP Request
     tls.write(
-        concat!(
+        concat!(git 
             "GET / HTTP/1.1\r\n",
             "Host:  www.wannacoffee.com\r\n",
             "Connection: close\r\n",
@@ -75,8 +80,15 @@ fn main() {
         .as_bytes(),
     )
     .unwrap();
+
     let mut plaintext = Vec::new();
     tls.read_to_end(&mut plaintext).unwrap();
     io::stdout().write_all(&plaintext).unwrap();
-    
+
+
+    let duration = start.elapsed();
+
+    println!("Total time taken for {} requests: {:?}", num_requests, duration);
+
+    // Ok(())
 }
